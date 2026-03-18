4f496bde906f24843e5a21b283b16359744d0cb6
ankush <raneankush93@gmail.com>
2026-03-18 23:47:00 +0530

    : env example and folder structure

---
+++ core/config.py
@@ -0,0 +1,31 @@
+from pydantic_settings import BaseSettings
+from functools import lru_cache
+
+class Settings(BaseSettings):
+    GOOGLE_CLIENT_ID : str 
+    GOOGLE_SECRET : str 
+    GOOGLE_REDIRECT_URL : str
+
+    SECRET_KEY : str 
+    REFRESh_TOKEN_EXPIRE_DAYS : int = 7
+    ALGORITHM : str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES : int = 15
+
+    DATABASE_URL : str 
+
+    REDIS_URL : str 
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
+
+settings = get_settings()
+
+
+
+
