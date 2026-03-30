5aa9ba45c0702d6661df315ce92d8da21f734972
ankush <raneankush93@gmail.com>
2026-03-30 23:18:33 +0530

    fix: refresh token rotation, logout

--- app/router/auth_routers.py
+++ app/router/auth_routers.py
@@ -1,14 +1,14 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends, Header, HTTPException 
 from sqlalchemy.orm import Session
 from fastapi.responses import RedirectResponse
 from app.core.config import settings
 from app.database.postgres import get_db
-from app.utils import oauth_client as oc
-from app.services import auth_service as a
-from app.core import security as s
-from app.schemas import Oauth_schema as ons
-from app.services import token_service as ts
-
+from app.utils import oauth_client 
+from app.services import auth_service 
+from app.core import security 
+from datetime import datetime, timezone
+from app.services import token_service 
+from app.schemas.Oauth_schema import RefreshRequest, TokenResponse
 
 router = APIRouter(prefix="/auth/google", tags =["auth"])
 
@@ -27,23 +27,62 @@ def google_login():
 #oauth_client = oc, auth_service = a, security = s, oauth_schema = os
 async def google_callback(code:str, db:Session = Depends(get_db)):
     try:
-        access_token = await oc.exchange_code_for_token(code)
+        access_token = await oauth_client.exchange_code_for_token(code)
         print("access_token : ", access_token)
     except Exception as e:
         print("error:", e)
         raise
 
     
-    get_user_data = await oc.get_google_user(access_token)
+    get_user_data = await oauth_client.get_google_user(access_token)
     user_info = get_user_data
 
-    get_or_create = a.get_or_create_user(db=db,google_user=user_info)
+    get_or_create = auth_service.get_or_create_user(db=db,google_user=user_info)
 
-    create_access =s.create_access_token(get_or_create.id)
-    create_refresh =s.create_refresh_token(get_or_create.id)
+    create_access =security.create_access_token(get_or_create.id)
+    create_refresh = security.create_refresh_token(get_or_create.id)
 
-    ts.store_refresh_token(get_or_create.id, create_refresh)
-    return ons.TokenResponse(access_token=create_access,refresh_token=create_refresh,token_type="bearer")
+    token_service.store_refresh_token(get_or_create.id, create_refresh)
+    return auth_service.TokenResponse(access_token=create_access,refresh_token=create_refresh,token_type="bearer")
+
+
+@router.post("/refresh", response_model = TokenResponse )
+def refresh_logic(request:RefreshRequest):
+    decode_token = security.decode_token(request)
+    token_service.verify_refresh_token(decode_token["sub"],request)
+    token_service.delete_refresh_token(decode_token["sub"])
+    create_refresh = security.create_refresh_token(decode_token["sub"])
+    create_access = security.create_access_token(decode_token["sub"])
+    token_service.store_refresh_token(decode_token["sub"], create_refresh)
+    return {
+        "access_token":create_access,
+        "refesh_token":create_refresh
+    }
+
+@router.post("/logout")
+def logout(authorization: str = Header()):
+    try:
+        scheme,access = authorization.split()
+        if scheme != "bearer":
+            raise Exception()
+    except ValueError:
+        raise HTTPException(
+            status_code = 401,
+            detail = "Invalid token"
+        )
+    decode = security.decode_token(access)
+    remain_ttl = decode["exp"] - datetime.now().timestamp()
+    if remain_ttl <0:
+        remain_ttl = 0
+    token_service.blacklist_token(decode["jti"],remain_ttl)
+    token_service.delete_refresh_token(decode["sub"])
+    return {
+        "message":"logged out"
+    }
+
+    
+
+
 
 
 
